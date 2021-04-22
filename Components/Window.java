2cb3c587dfbf64011c23214a5058ccde5bc190b4
skyhi179319 <39777622+skyhi179319@users.noreply.github.com>
2021-04-22 12:54:31 -0500

    initial commit

---
+++ Components/Window.java
@@ -0,0 +1,19 @@
+package Components;
+import java.awt.BorderLayout;
+import java.awt.Component;
+
+import javax.swing.JFrame;
+
+import Colors.colors;
+
+public class Window extends JFrame{
+	public Window(int X,int Y , int Width,int Height,String Title,Component North,Component East,Component South,Component West,Component Center) {
+		super.setBounds(X, Y, Width, Height);
+		super.setTitle(Title);
+		super.getContentPane().add(North,BorderLayout.NORTH);
+		super.getContentPane().add(East,BorderLayout.EAST);
+		super.getContentPane().add(South,BorderLayout.SOUTH);
+		super.getContentPane().add(West,BorderLayout.WEST);
+		super.getContentPane().add(Center,BorderLayout.CENTER);
+	}
+}
